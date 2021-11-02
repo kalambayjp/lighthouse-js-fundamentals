07dc35d86a3043ee9a9b040cda35060f0ea4cacb
Jp <kalambayjp@gmail.com>
2021-11-02 21:50:18 +0000

    loppylighthouse activity

---
+++ loopy-lighthouse.js
@@ -0,0 +1,15 @@
+let number = 100;
+
+for (number = 100; number <= 200; ++number) {
+  if (number % 3 === 0) {
+    if  (number % 4 === 0) {
+      console.log("LoopyLighthouse");
+    } else {
+      console.log("Loopy");
+    }
+  } else if (number % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(number);
+  }
+}
